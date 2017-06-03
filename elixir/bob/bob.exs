c133c92949ad9d8e71d301efd2e933f20b7e0364
Alex Zubiaga <a.zubiaga@controlexpert.com>
2017-06-03 15:26:18 +0200

    bob cond

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,9 +1,9 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        String.ends_with?(input, "?") -> "Sure."
-        String.trim(input) == "" -> "Fine. Be that way!"
-        String.upcase(input) == input -> "Whoa, chill out!"
+        String.match?(input, ~r/\?$/) -> "Sure."
+        String.match?(input, ~r/^\s*$/) -> "Fine. Be that way!"
+        String.match?(input, ~r/^[\P{Ll}]*[\p{Lu}][\P{Ll}]*$/u)-> "Whoa, chill out!"
         true -> "Whatever."
     end
   end
