25758d4ecffd35e3106c3977770788158f3166dc
Alex Zubiaga <alexzubiaga@users.noreply.github.com>
2017-06-04 10:02:22 +0200

    small performance enhancement

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -30,12 +30,16 @@ defmodule ProteinTranslation do
     |> Stream.map(&to_string/1)
     |> Stream.map(&of_codon/1)
     |> Enum.reduce_while({:ok, []}, &sequencer/2)
+    |> (fn
+        {:ok, l} -> {:ok, Enum.reverse l}
+        x -> x
+      end).()
   end
 
   @spec sequencer({atom, String.t()}, {atom, list(String.t())}) :: {atom, {atom, list(String.t())}}
   defp sequencer({:ok, "STOP"}, result), do: {:halt, result}
   defp sequencer({:error, _}, _), do: {:halt, {:error, "invalid RNA"}}
-  defp sequencer({:ok, codon}, {_, strain}), do: {:cont, {:ok, strain ++ [codon]}}
+  defp sequencer({:ok, codon}, {_, strain}), do: {:cont, {:ok, [codon | strain]}}
   
 
   @doc """
