1b611b15cb892e047e9dbe2892bc66d1c8de928b
Alex Zubiaga <alexzubiaga@users.noreply.github.com>
2017-06-04 08:50:47 +0200

    encode

---
+++ elixir/run-length-encoding/rle.exs
@@ -0,0 +1,26 @@
+defmodule RunLengthEncoder do
+  @doc """
+  Generates a string where consecutive elements are represented as a data value and count.
+  "AABBBCCCC" => "2A3B4C"
+  For this example, assume all input are strings, that are all uppercase letters.
+  It should also be able to reconstruct the data into its original form.
+  "2A3B4C" => "AABBBCCCC"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(string) do
+    string
+    |> String.graphemes
+    |> Stream.chunk_by(&(&1))
+    |> Stream.map(fn
+        [] -> ""
+        [i] -> i
+        [i|l] -> "#{Enum.count([i|l])}#{i}"
+      end)
+    |> Enum.join()
+  end
+
+  @spec decode(String.t) :: String.t
+  def decode(string) do
+
+  end
+end
