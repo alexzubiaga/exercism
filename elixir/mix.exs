f877b9b2eeff3ed7ea61318052d1c513abbd7b4a
Alex Zubiaga <alexzubiaga@users.noreply.github.com>
2017-06-05 19:57:45 +0200

    credo

---
+++ elixir/mix.exs
@@ -0,0 +1,21 @@
+defmodule Exercism do
+    use Mix.Project
+
+    def project do
+        [app: :exercism,
+        version: "0.1.0",
+        elixir: "~> 1.4",
+        start_permanent: Mix.env == :prod,
+        deps: deps()]
+    end
+
+    def application do
+        []
+    end
+
+    defp deps do
+    [
+        {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+    end
+end
