83ae4fec3942dead49fd7bc80a5167b09cdd6cbf
Alex Zubiaga <alexzubiaga@users.noreply.github.com>
2017-06-18 11:35:26 +0200

    added external resources

--- elixir/lib/exercism.ex
+++ elixir/lib/exercism.ex
@@ -1,8 +1,10 @@
 defmodule Exercism do
-    @doc """
+    @moduledoc """
     Automatically loads all exercise scripts.
     """
     for exercise <- Path.wildcard("*/*_test.exs") do
+        @external_resource Path.expand(exercise)
+        @external_resource Path.expand(String.replace(exercise, "_test.exs", ".exs"))
         file = Path.basename exercise
         directory = exercise |> Path.expand |> Path.dirname
         Code.require_file file, directory
